2dfa53d7f837c877145e54128c1d27ad81837c0a
Gustavo Schmaltz <gustavo.schmaltz@ZANAN.LOCAL>
2020-06-17 22:05:30 -0300

    Adicionando required nas voltagens de entrada e saidas da classe equipamento

---
+++ gPratical/Classes/Equipamentos.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gPratical.Classes
+{
+    public abstract class Equipamentos
+    {
+        public class Unit
+        {
+            [Required(ErrorMessage = "Tipo do equipamento é obrigatório")]
+            public string Tipo { get; set; }
+
+            [Required(ErrorMessage = "Marca do equipamento é obrigatório")]
+            public string Marca { get; private set; }
+
+            [Required(ErrorMessage = "Modelo do equipamento é obrigatório")]
+            public string Modelo { get; private set; }
+
+            [Required(ErrorMessage = "Local é obrigatório")]
+            public string Local { get; private set; }
+
+            [Required(ErrorMessage = "Voltagem de entrada é obrigatório")]
+            public string VoltagemEntrada { get; private set; }
+
+            [Required(ErrorMessage = "Voltagem de saida é obrigatório")]
+            public string VoltagemSaida { get; private set; }
+
+            public string SerialNumber { get; private set; }
+
+            public string Potencia { get; private set; }
+
+            public string Validade{ get; private set; }
+
+            public void ValidaClasse()
+            {
+                ValidationContext context = new ValidationContext(this, serviceProvider: null, items: null);
+                List<ValidationResult> results = new List<ValidationResult>();
+                bool isValid = Validator.TryValidateObject(this, context, results, true);
+
+                if (isValid == false)
+                {
+                    StringBuilder sbrErrors = new StringBuilder();
+                    foreach (var validationResult in results)
+                    {
+                        sbrErrors.AppendLine(validationResult.ErrorMessage);
+                    }
+                    throw new ValidationException(sbrErrors.ToString());
+                }
+            }
+        }
+        public class List
+        {
+            public List<Unit> ListUnit { get; set; }
+        }
+    }
+}
---
+++ gPratical/Classes/Usuario.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gPratical.Classes
+{
+    public abstract class Usuario
+    {
+        public class Unit
+        {
+            [Required(ErrorMessage = "Nome do Cliente é obrigatório")]
+            [StringLength(50, ErrorMessage = "Nome do Cliente deve ter no máximo 100 caracteres")]
+            public string Nome { get; set; }
+
+            [Required(ErrorMessage = "Data de nascimento é obrigatório")]
+            public DateTime DataDeNascimento { get; private set; }
+
+            [Required(ErrorMessage = "Telefone é obrigatório")]
+            public string Telefone { get; private set; }
+
+            [Required(ErrorMessage = "Cargo é obrigatório")]
+            public string Cargo { get; private set; }
+
+            [Required(ErrorMessage = "Setor é obrigatório")]
+            public string Setor { get; private set; }
+
+            [Required(ErrorMessage = "Endereco é obrigatório")]
+            public string Endereco { get; private set; }
+
+            [Required(ErrorMessage = "Estado é obrigatório")]
+            public string Estado { get; private set; }
+
+            [Required(ErrorMessage = "Cidade é obrigatório")]
+            public string Cidade { get; private set; }
+
+            [Required(ErrorMessage = "Bairro é obrigatório")]
+            public string Bairro { get; private set; }
+
+            [Required(ErrorMessage = "Genero é obrigatório")]
+            public string Genero { get; private set; }
+
+            public void ValidaClasse()
+            {
+                ValidationContext context = new ValidationContext(this, serviceProvider: null, items: null);
+                List<ValidationResult> results = new List<ValidationResult>();
+                bool isValid = Validator.TryValidateObject(this, context, results, true);
+
+                if (isValid == false)
+                {
+                    StringBuilder sbrErrors = new StringBuilder();
+                    foreach (var validationResult in results)
+                    {
+                        sbrErrors.AppendLine(validationResult.ErrorMessage);
+                    }
+                    throw new ValidationException(sbrErrors.ToString());
+                }
+            }
+        }
+        public class List
+        {
+            public List<Unit> ListUnit { get; set; }
+        }
+    }
+}
